3a0a703bb3e8efc6af55d5404457d55229c8d601
joelleishimwe <joelle.ishimwe@ocsbstudent.ca>
2018-03-06 10:16:25 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,7 @@
+-- Title: TouchAndDrag
+-- Name: Joelle Ishimwe
+-- Course: ICS2O
+-- This program displays images that can be touched and draged 
+-----------------------------------------------------------------------------------------
+
+-- Your code here
